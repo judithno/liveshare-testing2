34f3ece42e5ee1d7751673273275c71d59eb68ea
jackiedai <jackie123768@gmail.com>
2023-11-19 04:10:37 -0800

    Add theatre.js comments

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,17 @@ import { editable as e, SheetProvider } from '@theatre/r3f'
 import { PerspectiveCamera } from "@theatre/r3f";
 import demoProjectState from './state.json'
 
+/* Delete to remove studio UI */
 studio.initialize();
 studio.extend(extension);
+/* ---------- */
 
+// stores animation/object states 
 const demoSheet = getProject('Demo Project', {state: demoProjectState}).sheet('Demo Sheet');
 
 
 const App = () => {
+  // plays animation for two seconds infintite times
   useEffect(() => {
     demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 2] }));
     console.log("playing");
@@ -32,14 +36,18 @@ const App = () => {
         <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75}/>
         <ambientLight />
         <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+
+        // objects in scene
         <e.mesh theatreKey="Cube">
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="orange" />
         </e.mesh>
-        <e.mesh theatreKey="Floor" >
+
+        <e.mesh theatreKey="Floor">
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color={"pink"} />
         </e.mesh>
+
       </SheetProvider>
     </Canvas>
   );
